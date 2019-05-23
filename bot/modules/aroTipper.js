ea4915b4f4b51ea2709046414efc6fbdf4204d58
Kevin <50747021+kdrent015@users.noreply.github.com>
2019-05-23 16:12:30 +0200

    Update aroTipper.js

--- bot/modules/aroTipper.js
+++ bot/modules/aroTipper.js
@@ -1,15 +1,3 @@
-/*
-Simply find and replace instances below with the coin and symbol you want to use!
-search and replace with case sensitivity!!
-example:
-1. Arionum   = Ethereum
-2. aro        = eth
-3. ARO        = ETH
-4. http://Explorer-Url/tx/
-5. http://Explorer-Url/address/
-*/
-
-/*
 'use strict';
 
 const bitcoin = require('bitcoin');
